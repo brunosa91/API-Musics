6ec6791456b0f276450bb3117a5af75ca7fe8f88
Bruno Sa <brunosantana.engenharia@gmail.com>
2024-05-14 12:39:59 -0300

    feature/cria o método authentication manager na classe de config do spring security para através do authentication configuration pegar a instância de manager para ser lido na controller, também implementado o hash de senha

--- src/main/java/com/reproduction/musics/config/SecurityConfigurations.java
+++ src/main/java/com/reproduction/musics/config/SecurityConfigurations.java
@@ -3,9 +3,13 @@ package com.reproduction.musics.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,5 +26,12 @@ public class SecurityConfigurations {
                         .requestMatchers(HttpMethod.POST,"/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST,"/list").hasRole("USER")
                         .anyRequest().authenticated()).build();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
