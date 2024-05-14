7e3ff567e7d54aef902f4505082c9f2c5be24bcd
Bruno Sa <brunosantana.engenharia@gmail.com>
2024-05-14 12:35:12 -0300

    feature/implementado o método de login na controller e injetado o authentication manager do security que tem por função autenticar um userName passwor que pega uma instância do repository

--- src/main/java/com/reproduction/musics/controller/AuthenticationController.java
+++ src/main/java/com/reproduction/musics/controller/AuthenticationController.java
@@ -1,9 +1,28 @@
 package com.reproduction.musics.controller;
 
+import com.reproduction.musics.dto.AuthenticationDto;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("auth")
 public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody @Valid AuthenticationDto authDto){
+        var userNamePassword = new UsernamePasswordAuthenticationToken(authDto.getLogin(),authDto.getPassword());
+        var auth = this.authenticationManager.authenticate(userNamePassword);
+        return ResponseEntity.ok().build();
+
+    }
+
 }
